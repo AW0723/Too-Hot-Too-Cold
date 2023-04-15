76236423f6527cfbbf8261fbf9f0439e087a44f3
AW0723 <64997746+AW0723@users.noreply.github.com>
2023-04-15 01:43:48 -0700

    Add project files.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player.MoveHorizontal(Input.GetAxis("Horizontal"));
+        //if(Input.GetButtonDown("Jump"))
+        //{
+        //    player.Jump();
+        //}
+    }
+}
